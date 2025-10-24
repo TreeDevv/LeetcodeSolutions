12e5cccc0692a3caf7083e61fb4faa300b1b694c
Hunter Paul <119270465+TreeDevv@users.noreply.github.com>
2025-10-23 19:06:04 -0500

    Implement search in binary search tree

---
+++ easy/700-SearchBinaryTree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* curNode = root;
+        while (curNode != nullptr) {
+            if (val > curNode->val) curNode=curNode->right;
+            else if (val < curNode->val) curNode=curNode->left;
+            else if (val == curNode->val) return curNode; 
+        }
+
+        return nullptr;
+    }
+};
